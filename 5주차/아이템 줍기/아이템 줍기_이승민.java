d27c0bafe334479ff762e5285c4d0b3df36a6348
Nooby213 <lsm5631@naver.com>
2024-10-10 02:29:45 +0900

    5주차 이승민

--- 5주차/아이템 줍기/아이템 줍기_이승민.java
+++ 5주차/아이템 줍기/아이템 줍기_이승민.java
@@ -4,37 +4,32 @@ class Solution {
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int[] dy = new int[]{-1, 1, 0, 0};
         int[] dx = new int[]{0, 0, -1, 1};
-        int[][] rec = new int[102][102]; // 좌표 2배로 확장
+        int[][] rec = new int[101][101];
 
-        // 2배로 확장하여 그리기 (정확한 경계선을 구분하기 위함)
         for(int i = 0; i < rectangle.length; i++) {
             int x1 = rectangle[i][0] * 2;
             int y1 = rectangle[i][1] * 2;
             int x2 = rectangle[i][2] * 2;
             int y2 = rectangle[i][3] * 2;
 
-            // 경계선 그리기
             for (int x = x1; x <= x2; x++) {
-                if (rec[y1][x] != -1) rec[y1][x] = 1; // 위쪽 경계선
-                if (rec[y2][x] != -1) rec[y2][x] = 1; // 아래쪽 경계선
+                if (rec[y1][x] != -1) rec[y1][x] = 1;
+                if (rec[y2][x] != -1) rec[y2][x] = 1;
             }
             for (int y = y1; y <= y2; y++) {
-                if (rec[y][x1] != -1) rec[y][x1] = 1; // 왼쪽 경계선
-                if (rec[y][x2] != -1) rec[y][x2] = 1; // 오른쪽 경계선
+                if (rec[y][x1] != -1) rec[y][x1] = 1;
+                if (rec[y][x2] != -1) rec[y][x2] = 1;
             }
 
-            // 내부를 -1로 표시
-            for (int y = y1 + 1;
-            y < y2; y++) {
+            for (int y = y1 + 1; y < y2; y++) {
                 for (int x = x1 + 1; x < x2; x++) {
                     rec[y][x] = -1;
                 }
             }
         }
 
-        // BFS 탐색
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{characterX * 2, characterY * 2, 0}); // 시작점도 2배로 변환
+        q.add(new int[]{characterX * 2, characterY * 2, 0});
         rec[characterY * 2][characterX * 2] = 0;
 
         while(!q.isEmpty()) {
@@ -44,7 +39,7 @@ class Solution {
             int t = now[2];
 
             if (nowX == itemX * 2 && nowY == itemY * 2) {
-                return t / 2; // 결과를 다시 절반으로 나누어 반환
+                return t / 2;
             }
 
             for(int d = 0; d < 4; d++) {
@@ -53,7 +48,7 @@ class Solution {
 
                 if (nextX >= 1 && nextX <= 100 && nextY >= 1 && nextY <= 100 && rec[nextY][nextX] == 1) {
                     q.add(new int[]{nextX, nextY, t+1});
-                    rec[nextY][nextX] = 0; // 방문한 경로는 0으로 표시하여 다시 방문하지 않게 함
+                    rec[nextY][nextX] = 0;
                 }
             }
         }
