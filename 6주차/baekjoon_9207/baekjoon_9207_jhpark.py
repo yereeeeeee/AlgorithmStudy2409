2054aaefb763112caa57d07648c5cbb7f074a17d
jhpark0323 <brianwogus@naver.com>
2024-10-22 21:00:18 +0900

    1022_jhpark

---
+++ 6주차/baekjoon_9207/baekjoon_9207_jhpark.py
@@ -0,0 +1,17 @@
+import sys
+from pprint import pprint
+input = sys.stdin.readline
+n = int(input())
+
+# def back(cnt):
+#     for i in range(5):
+#         for j in range(9):
+
+
+
+
+for test_case in range(n):
+    arr = [list(input().strip()) for _ in range(5)]
+    input()
+    pprint(arr)
+
