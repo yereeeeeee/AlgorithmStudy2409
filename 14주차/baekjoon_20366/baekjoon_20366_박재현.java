63e9d9d1a6f410198a438a71c1301d73af8712c1
jhpark0323 <brianwogus@naver.com>
2025-01-21 15:44:05 +0900

    0121

---
+++ 14주차/baekjoon_20366/baekjoon_20366_박재현.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int snowman1 = arr[i] + arr[j];
+
+                int left = 0;
+                int right = n-1;
+
+                while (left < right) {
+                    // 같은 눈덩이 선택시 패스
+                    if (left == i || left == j) {
+                        left++;
+                        continue;
+                    }
+                    if (right == i || right == j) {
+                        right--;
+                        continue;
+                    }
+
+                    int snowman2 = arr[left] + arr[right];
+                    ans = Math.min(ans, Math.abs(snowman1 - snowman2));
+
+                    // 0나오면 끝
+                    if (ans == 0) {
+                        System.out.println(0);
+                        return;
+                    }
+
+                    if (snowman1 < snowman2) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+
+                }
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+}
